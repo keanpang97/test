2d36bef4faaa5d2fae09f24514ef000afd94706b
keanpang97 <61856282+keanpang97@users.noreply.github.com>
2020-03-09 17:55:33 +0800

    go 42th commit

--- hello.go
+++ hello.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	for {
-		time.Sleep(5 * time.Second)
-		fmt.Println("Hello World")
-		fmt.Println("Hello World")
-	}
+	fmt.Println("Hello World")
 }
