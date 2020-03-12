087b936f2dbd1ea5e4f658612ee411b330875ea2
keanpang97 <61856282+keanpang97@users.noreply.github.com>
2020-03-12 16:54:21 +0800

    go 123th commit

--- hello.go
+++ hello.go
@@ -5,5 +5,24 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
+	greeting := hello("Kean Pang")
+	fmt.Println(greeting)
+
+	total := sum(4, 5)
+	fmt.Println(total)
+}
+
+func hello(user string) string {
+	if len(user) == 0 {
+		return "Hello Dude!"
+	}
+
+	return fmt.Sprintf("Hello %v", user)
+}
+
+//addition of two number
+func sum(a, b int) int {
+	total := a + b
+
+	return total
 }
---
+++ hello_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+//TestHello is to test hello function
+func TestHello(t *testing.T) {
+
+	stringReturn := hello("abc")
+
+	assert.Equal(t, stringReturn, "Hello abc", "function hello is not functioning well.")
+}
+
+//TestSum is to test sum function
+func TestSum(t *testing.T) {
+
+	numberReturn := sum(3, 3)
+
+	if numberReturn != 6 {
+		t.Errorf("IntMin(3, 3) = %d; 6 is the correct answer", numberReturn)
+	}
+}
